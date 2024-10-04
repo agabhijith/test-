eb0b6868ebe66445e693cb08533ee275ed92a9e1
agabhijith <agabhijith2028@cs.sjcetpalai.ac.in>
2024-10-04 16:17:27 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+print("Hello N/World!")
+x ="Python "
+y = "is "
+z = "awesome "
+print (x +y +z )
+username = input ( "username :")
+print("username is : " + username
+      ''''
+a = "Hello"
+b = "World"
+c =  a + b
+print (c)
+      '''
