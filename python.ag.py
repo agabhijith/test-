135240fc70448c5e24be81cf41a46aff84ff9db0
agabhijith <agabhijith2028@cs.sjcetpalai.ac.in>
2024-10-18 14:36:15 +0530

    Add files via upload

---
+++ python.ag.py
@@ -0,0 +1,9 @@
+'''
+author ag abhijith
+date 18-10-24
+description;programme that asks the user to input their name and age. after recieving the  input ,display a message that includes the
+user,s name and age .
+'''
+name=input("enter your name")
+age=input("enter your age")
+print("Hello,",name , "you are" ,age, 'years old')
