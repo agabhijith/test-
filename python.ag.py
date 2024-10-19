b3e9f57b3d51955b349e1881f9601ceb62601bbd
agabhijith <agabhijith2028@cs.sjcetpalai.ac.in>
2024-10-19 14:43:20 +0530

    Add files via upload

--- python.ag.py
+++ python.ag.py
@@ -1,9 +1,18 @@
-'''
-author ag abhijith
-date 18-10-24
-description;programme that asks the user to input their name and age. after recieving the  input ,display a message that includes the
-user,s name and age .
-'''
-name=input("enter your name")
-age=input("enter your age")
-print("Hello,",name , "you are" ,age, 'years old')
+''' author ag abhijith
+date 19/10/2024
+
+''''''num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+num3=int(input("enter the third number"))
+add=num1+num2
+print("the sum of num1 and num2 is;",add)
+sub=num2-num1
+print("The difference when num2 is subtracted from num1 is:",sub)
+multiplication=num1*num2
+print("The product of num1 and num2 is",multiplication)
+division=num1/num2
+print("The quotient when num1 is divided by num2 is :",division)
+mod=num1%num2
+print("The remainder when num1 is divided by num2 is:",mod)
+result = (num1 + num2) * num3 / 2
+print("the result of (num1 + num2) * num3 / 2",result)
