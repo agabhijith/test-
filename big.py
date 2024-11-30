9755d8ba5b2dedb5a374f250d2dbfe74154b83f5
agabhijith <agabhijith2028@cs.sjcetpalai.ac.in>
2024-11-30 16:17:30 +0530

    Add files via upload

---
+++ big.py
@@ -0,0 +1,9 @@
+def maximum():
+    numbers = []
+    for i in range(3):
+        number=int(input("enter the number"))
+        numbers.append(number)
+    print("the largest among three numbers is:",max(numbers))
+maximum()
+
+
